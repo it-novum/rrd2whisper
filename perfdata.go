0a993e1c6fad65aa6f07c734a96bd872902b1b62
Johannes Drummer <j.drummer@thelambda.de>
2019-07-23 12:51:58 +0200

    fix some perfdata parser bugs

--- perfdata.go
+++ perfdata.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"strconv"
 	"strings"
 	"fmt"
@@ -17,56 +18,64 @@ type Perfdata struct {
 	critical float64
 }
 
-var perfdataSplitRegex *regexp.Regexp
-var perfdataLabelRegex *regexp.Regexp
-var perfdataValueRegex *regexp.Regexp
-
-func init() {
-	perfdataSplitRegex = regexp.MustCompile(`(('[^']+')?[^\s]+)`)
-	perfdataLabelRegex = regexp.MustCompile(`^(('[^']+')?("[^"]+")?([^=])*)`)
-	perfdataValueRegex = regexp.MustCompile(`^(\d+[\.,]?\d*)`)
-}
+var perfdataSplitRegex = regexp.MustCompile(`(('[^']+')?[^\s]+)`)
+var perfdataLabelRegex = regexp.MustCompile(`^(('[^']+')?([^='])*)`)
+var perfdataValueRegex = regexp.MustCompile(`^(\d+[\.,]?\d*)`)
 
 func perfdataParseValue(valueStr string) (*Perfdata, error) {
 	var err error
-	pd := new(Perfdata)
+	pd := &Perfdata{
+		label: "",
+		value: math.NaN(),
+		uom: "",
+		warning: math.NaN(),
+		critical: math.NaN(),
+		min: math.NaN(),
+		max: math.NaN(),
+	}
 	data := strings.Split(valueStr, ";")
 	dataLen := len(data)
 	if dataLen < 1 || dataLen > 5{
-		return nil, fmt.Errorf("Invalid perfdata value: %s", valueStr)
+		return nil, fmt.Errorf("invalid perfdata value: %s", valueStr)
 	}
 	// 'label'=value[UOM];[warn];[crit];[min];[max]
 	if dataLen == 5 && data[4] != "" {
 		if pd.max, err = strconv.ParseFloat(data[4], 64); err != nil {
-			return nil, fmt.Errorf("Could not parse max value \"%s\": %s", data[4], err)
+			return nil, fmt.Errorf("could not parse max value \"%s\": %s", data[4], err)
 		}
 	}
 	if dataLen >= 4 && data[3] != "" {
 		if pd.min, err = strconv.ParseFloat(data[3], 64); err != nil {
-			return nil, fmt.Errorf("Could not parse min value \"%s\": %s", data[3], err)
+			return nil, fmt.Errorf("could not parse min value \"%s\": %s", data[3], err)
 		}
 	}
 	if dataLen >= 3 && data[2] != "" {
 		if pd.critical, err = strconv.ParseFloat(data[2], 64); err != nil {
-			return nil, fmt.Errorf("Could not parse critical value \"%s\": %s", data[2], err)
+			return nil, fmt.Errorf("could not parse critical value \"%s\": %s", data[2], err)
 		}
 	}
 	if dataLen >= 2 && data[1] != "" {
 		if pd.warning, err = strconv.ParseFloat(data[1], 64); err != nil {
-			return nil, fmt.Errorf("Could not parse warning value \"%s\": %s", data[1], err)
+			return nil, fmt.Errorf("could not parse warning value \"%s\": %s", data[1], err)
 		}
 	}
 	pd.label = perfdataLabelRegex.FindString(data[0])
 	if pd.label == "" {
-		return nil, fmt.Errorf("Invalid label value: %s", data[0])
+		return nil, fmt.Errorf("invalid label: %s", data[0])
+	}
+	if len(pd.label) == len(data[0]) {
+		return nil, fmt.Errorf("no value found: %s", data[0])
+	}
+	if data[0][len(pd.label)] != '=' {
+		return nil, fmt.Errorf("invalid format %s", data[0])
 	}
 	valueWithUnit := data[0][len(pd.label)+1:]
 	rawval := perfdataValueRegex.FindString(valueWithUnit)
 	if rawval == "" {
-		return nil, fmt.Errorf("Invalid decimal in: %s", data[0])
+		return nil, fmt.Errorf("invalid decimal in: %s", data[0])
 	}
 	if pd.value, err = strconv.ParseFloat(rawval, 64); err != nil {
-		return nil, fmt.Errorf("Could not parse value \"%s\": %s", data[0], err)
+		return nil, fmt.Errorf("could not parse value \"%s\": %s", data[0], err)
 	}
 
 	if len(rawval) == len(valueWithUnit) {
@@ -82,12 +91,12 @@ func parsePerfdata(perfdata string) ([]*Perfdata, error) {
 	var err error
 	valueStrings := perfdataSplitRegex.FindAllString(perfdata, -1)
 	if len(valueStrings) == 0 {
-		return nil, fmt.Errorf("Could not split perfdata into values: %s", perfdata)
+		return nil, fmt.Errorf("could not split perfdata: %s", perfdata)
 	}
 	values := make([]*Perfdata, len(valueStrings))
 	for i, valueStr := range valueStrings {
 		if values[i], err = perfdataParseValue(valueStr); err != nil {
-			return nil, fmt.Errorf("could not parse perfdata value: %s", err)
+			return nil, fmt.Errorf("could not parse perfdata: %s", err)
 		}
 	}
 	return values, nil
--- perfdata_test.go
+++ perfdata_test.go
@@ -1,44 +1,174 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"testing"
 )
 
+
+var testPerfdataSetOk = []struct{
+	in string
+	out []*Perfdata
+}{
+	{
+		in: "users=2;3;7;0",
+		out: []*Perfdata{
+			&Perfdata{
+				label: "users",
+				uom: "",
+				value: 2,
+				warning: 3,
+				critical: 7,
+				min: 0,
+				max: math.NaN(),
+			},
+		},
+	},
+	{
+		in: "load1=0.050;7.000;10.000;0; load5=0.040;6.000;7.000;0; load15=0.010;5.000;6.000;0;",
+		out: []*Perfdata{
+			&Perfdata{
+				label: "load1",
+				uom: "",
+				value: 0.050,
+				warning: 7.000,
+				critical: 10.000,
+				min: 0,
+				max: math.NaN(),
+			},
+			&Perfdata{
+				label: "load5",
+				uom: "",
+				value: 0.040,
+				warning: 6.000,
+				critical: 7.000,
+				min: 0,
+				max: math.NaN(),
+			},
+			&Perfdata{
+				label: "load15",
+				uom: "",
+				value: 0.010,
+				warning: 5.000,
+				critical: 6.000,
+				min: 0,
+				max: math.NaN(),
+			},
+		},
+	},
+	{
+		in: "'users'=2%;3;7;0",
+		out: []*Perfdata{
+			&Perfdata{
+				label: "'users'",
+				uom: "%",
+				value: 2,
+				warning: 3,
+				critical: 7,
+				min: 0,
+				max: math.NaN(),
+			},
+		},
+	},
+	{
+		in: "users=2;;;;",
+		out: []*Perfdata{
+			&Perfdata{
+				label: "users",
+				uom: "",
+				value: 2,
+				warning: math.NaN(),
+				critical: math.NaN(),
+				min: math.NaN(),
+				max: math.NaN(),
+			},
+		},
+	},
+}
+
+var testPerfdataSetFail = []struct{
+	in string
+	err string
+	out []*Perfdata
+}{
+	{
+		in: "users'=2%;3;7;0",
+		out: []*Perfdata{
+			&Perfdata{
+				label: "users'",
+				uom: "%",
+				value: 2,
+				warning: 3,
+				critical: 7,
+				min: 0,
+				max: math.NaN(),
+			},
+		},
+		err: "could not parse perfdata: invalid format users'=2%",
+	},
+	{
+		in: "users2%;3;7;0",
+		out: []*Perfdata{
+			&Perfdata{
+				label: "users",
+				uom: "",
+				value: 2,
+				warning: 3,
+				critical: 7,
+				min: 0,
+				max: math.NaN(),
+			},
+		},
+		err: "could not parse perfdata: no value found: users2%",
+	},
+}
+
+func testPerfdata(in string, out []*Perfdata) error {
+	result, err := parsePerfdata(in)
+	if err != nil {
+		return err
+	}
+	if len(result) != len(out) {
+		return fmt.Errorf("number of parsed perfdata %d != expected %d", len(result), len(out))
+	}
+	for i := 0; i < len(result); i++ {
+		if out[i].label != result[i].label {
+			return fmt.Errorf("label is not equal")
+		}
+		if out[i].uom != result[i].uom {
+			return fmt.Errorf("uom is not equal")
+		}
+		if !(out[i].warning == result[i].warning || (math.IsNaN(out[i].warning) && math.IsNaN(result[i].warning))) {
+			return fmt.Errorf("warning is not equal")
+		}
+		if !(out[i].critical == result[i].critical || (math.IsNaN(out[i].critical) && math.IsNaN(result[i].critical))) {
+			return fmt.Errorf("critical is not equal")
+		}
+		if !(out[i].min == result[i].min || (math.IsNaN(out[i].min) && math.IsNaN(result[i].min))) {
+			return fmt.Errorf("min is not equal")
+		}
+		if !(out[i].max == result[i].max || (math.IsNaN(out[i].max) && math.IsNaN(result[i].max))) {
+			return fmt.Errorf("max is not equal")
+		}
+	}
+	return nil
+}
+
 func TestParsePerfdata(t *testing.T) {
-	t.Run("basics", func(t *testing.T) {
-		perfdata, err := parsePerfdata(`'label1'=123%;23;40.23;50;2`)
-		if err != nil {
-			t.Error(err)
+	for _, pftest := range testPerfdataSetOk {
+		if err := testPerfdata(pftest.in, pftest.out); err != nil {
+			t.Errorf("in: %s err: %s", pftest.in, err)
 		}
-		if len(perfdata) != 1 {
-			t.Errorf("Unexpected number of perfdata in result: %d", len(perfdata))
+	}
+	for _, pftest := range testPerfdataSetFail {
+		err := testPerfdata(pftest.in, pftest.out)
+		if err == nil {
+			t.Errorf("in: %s expected error \"%s\" but it passed", pftest.in, pftest.err)
+		} else {
+			if err.Error() != pftest.err {
+				t.Errorf("in: %s expected error \"%s\" but got \"%s\"", pftest.in, pftest.err, err)
+			}
 		}
-		if perfdata[0].label != `'label1'` {
-			t.Errorf("Unexpected label %s != %s", `'label1'`, perfdata[0].label)
-		}
-		if perfdata[0].uom != "%" {
-			t.Errorf("Unexpected uom %s != %s", "%", perfdata[0].uom)
-		}
-		if perfdata[0].warning != float64(23) {
-			t.Errorf("Unexpected warning %s != %s", "%", perfdata[0].uom)
-		}
-		if perfdata[0].critical != float64(40.23) {
-			t.Errorf("Unexpected critical %s != %s", "%", perfdata[0].uom)
-		}
-		if perfdata[0].min != float64(50) {
-			t.Errorf("Unexpected min %s != %s", "%", perfdata[0].uom)
-		}
-		if perfdata[0].max != float64(2) {
-			t.Errorf("Unexpected max %s != %s", "%", perfdata[0].uom)
-		}
-	})
-	t.Run("multiple", func(t *testing.T) {
-		perfdata, err := parsePerfdata(`'labe l1'=123%;23;40;50;2 'lab%e l3'=123%;23;40;50;2 label3=23`)
-		if err != nil {
-			t.Error(err)
-		}
-		if len(perfdata) != 3 {
-			t.Errorf("Unexpected number of perfdata in result: %d", len(perfdata))
-		}
-	})
+	}
 }
