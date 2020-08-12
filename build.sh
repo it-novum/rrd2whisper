ed4db106d36419aa1632bf5485dbce4394ae367d
Johannes Drummer <j.drummer@thelambda.de>
2020-08-12 16:56:40 +0200

    upx does not work with focal

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ for distribution in bionic xenial trusty stretch buster focal; do
     if [ ! -z "$1" ] && [ "$1" != "$distribution" ]; then
         continue
     fi
+    
     # we use build/env to provide an empty context for docker
     docker build -t rrd2whisper_build:$distribution -f dockerfiles/Dockerfile.$distribution build/env
 
     docker run --rm -t -i -v $(pwd):/build/ -v $(pwd)/build/go:/usr/local/go rrd2whisper_build:$distribution /usr/local/go/bin/go build -ldflags "-X main.Version=$VERSION"
-    upx rrd2whisper
     mv rrd2whisper build/rrd2whisper_${VERSION}_${distribution}_amd64
 done
